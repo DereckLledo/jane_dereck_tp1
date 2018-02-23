a4435b5aa2259f8de79691bc2b4a1417e4b87a97
Jane-Mary Menassa <jane-mary_love@hotmail.com>
2018-02-22 19:10:22 -0500

    Ajout des class Clients et Plats et modification de la localisation du fichie chezBarette.txt

---
+++ TP1/src/partie2/Clients.java
@@ -0,0 +1,36 @@
+package partie2;
+
+public class Clients {
+	
+	private String nom;
+	private double montantTotal;
+	
+	public Clients(String nom){
+		
+		this.setNom(nom);
+		this.montantTotal = 0;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	
+	public double getMontantTotal() {
+		return montantTotal;
+	}
+
+	public void setMontantTotal(double montantTotal) {
+		this.montantTotal = montantTotal;
+	}
+	
+	/* pour ajouter une commande
+	public commande(String ){
+		
+	}*/
+
+}
---
+++ TP1/src/partie2/Plats.java
@@ -0,0 +1,31 @@
+package partie2;
+
+public class Plats {
+	
+	private String plat;
+	private double prix;
+	
+	public Plats(String plat, double prix){
+		
+		this.plat = plat;
+		this.prix = prix;
+
+	}
+
+	public String getPlat() {
+		return plat;
+	}
+
+	public void setPlat(String plat) {
+		this.plat = plat;
+	}
+
+	public double getPrix() {
+		return prix;
+	}
+
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+
+}
