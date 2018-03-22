3d65fd9b2180bcd7d24017aac91fcdeb60ea5043
derecklledo <derecklledo@gmail.com>
2018-03-22 19:57:16 -0400

    test sur verification de commande arreter - Incapable de résoudre problème

--- TP1/src/test/TestUnit.java
+++ TP1/src/test/TestUnit.java
@@ -1,28 +1,32 @@
 package test;
+import java.util.ArrayList;
+
 import partie3.*;
 import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.*;
-import org.mockito.Mockito;
-import org.mockito.Mock;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
+
 
 public class TestUnit {
 
 	ChezBarette chezB;
 
-	@Mock
-	Plats plat;
-	
-	@Mock
+	Plats plat;	
 	Clients cli;
 	
 	
 	@Before
 	public void AvantChaqueTest() {
 		chezB = new ChezBarette();
+		cli = Mockito.mock(Clients.class);
+		plat = Mockito.mock(Plats.class);
 
 	}
 
@@ -42,33 +46,60 @@ public class TestUnit {
 	
 	@Test
 	public void testVerifierCommandeFalse(){
-		//un string vide n'est pas un bon f
+		//un string vide n'est pas un bon format
 
-		String a = "";
-		assertFalse(chezB.verifierCommande(a));
+		String testCommande = "";
+		assertFalse(chezB.verifierCommande(testCommande));
 	}
 	
 
+	@Test
+	public void testVerifierClientTrue(){
+		// un string sans espace 
+		String nomTest = "Jean";
+		assertTrue(chezB.verifierClient(nomTest));
+		
+	}
 	
 	@Test
+	public void testVerifierClientFalse(){
+		// un string avec un espace
+		String nomTest = "Jean test";
+		assertFalse(chezB.verifierClient(nomTest));
+		
+	}
+	
+	
+	
+/*	@Test
 	public void testVerifierCommandeTrue(){
 		
-//FONCTIONNE PAS
+		Clients cli = mock(Clients.class);
+		String test = "testNom testPlat 10.65"; 
 		
-		//le format d'une commande est "client plat nbPlats"
-		String laCommande = "nomClient nomPlat 2";
-		String[] tabCommande = laCommande.split(" ");
-
+		when()
 		
 		assertTrue(chezB.verifierCommande(laCommande));
 		
-	/*	cli = new Clients("testNom");
+		cli = new Clients("testNom");
 		plat = new Plats("testPlat", 10.65);
-		String test = "testNom testPlat 10.65"; */
+		String test = "testNom testPlat 10.65"; 
+		Clients cli = new Clients("nomTest");
+		Plats plat = new Plats("platTest", 10);
+		
+		ArrayList<Clients> listeClients = new ArrayList<Clients>();
+		ArrayList<Plats> listePlats = new ArrayList<Plats>();
+		
+		listeClients.add(cli);
+		listePlats.add(plat);
+		
+		chezB.setListeClients(listeClients);
+		
+		String laCommande = "nomTest platTest 10";
 		
 		
 		//assertTrue(chezB.verifierCommande(test));
-	}
+	}*/
 	
 
 }
