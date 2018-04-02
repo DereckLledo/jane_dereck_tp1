984e25ca0103edcd95df4964ac8e5513f202b96c
Dereck Lledo <derecklledo@gmail.com>
2018-04-02 18:00:14 -0400

    calcul des taxes et des frais

--- TP1/src/partie3/ChezBarette.java
+++ TP1/src/partie3/ChezBarette.java
@@ -120,11 +120,12 @@ public class ChezBarette {
 			for (Clients client : table.getListeClients()) {
 				
 				prixAvantTaxes = client.getMontantTotal();
+				
 				totalAvantTaxes += prixAvantTaxes;
 				//on affiche seulement les clients avec une facture > 0$
 				if ( prixAvantTaxes > 0) {
 
-				prixApresTaxes = calculerPrix(prixAvantTaxes);
+				//prixApresTaxes = calculerPrix(prixAvantTaxes);
 				
 				
 				System.out.println(client.getNom() + ": " + f.format(client.getMontantTotal()) + "$ ");
@@ -133,10 +134,12 @@ public class ChezBarette {
 			}
 			
 			
-			totalTaxes = calculerTaxes(totalAvantTaxes);
-			table.montantFinal();
-			System.out.println("\nTAXES : " + f.format(totalTaxes) + "$ ");
-			System.out.println("Frais : " /*+ calulerFrais()*/ + "$ ");
+			
+			
+			table.setMontantTotal(totalAvantTaxes);
+			
+			System.out.println("\nTAXES : " + f.format(table.getMontantTaxes()) + "$ ");
+			System.out.println("Frais : " + f.format(table.getFrais()) + "$ ");
 			System.out.println("\nTOTAL: " + f.format(table.getMontantTotal()) + "$ ");
 			System.out.println("\n**UN FRAIS DE SERVICE DE 15% S'AJOUTE LORSQUE LE MONTANT DE LA FACTURE DÉPASSE 100$ OU QU'IL Y A 3 CLIENTS ET PLUS À UNE TABLE**");
 
--- TP1/src/partie3/Tables.java
+++ TP1/src/partie3/Tables.java
@@ -8,11 +8,16 @@ public class Tables {
 	
 	private ArrayList<Clients> listeClients = new ArrayList<Clients>();
 	
-
+	private double montantAvantTaxes = 0;
 	private double montantTotal = 0;
 
 
 	private int numeroTable;
+
+
+	private double frais = 0;
+
+	private double montantTaxes = 0;
 	
 
 	public Tables(int noTable) {
@@ -37,20 +42,48 @@ public class Tables {
 	public double getMontantTotal() {
 		return montantTotal;
 	}
+	
+	//accesseur de frais
+	public double getFrais() {
+		return frais;
+	}
+	
+	//accesseur de taxes
+	public double getMontantTaxes() {
+		return montantTaxes;
+	}
 
 	//mutateur de montantTotal
-	public void setMontantTotal(double montantTotal) {
-		this.montantTotal += montantTotal;
+	public void setMontantTotal(double montantAvantTaxes) {
+		
+		this.montantAvantTaxes = montantAvantTaxes;
+		this.montantTaxes  = calculerTaxes(montantAvantTaxes);
+		
+		//on ajoute les taxes au prix
+		this.montantTotal = montantAvantTaxes + montantTaxes;
+		
+		
+		//si le montantTotal est plus grand que 100 ou qu'il y a plus de 2 clients, on ajoute des frais de 15% au prix avant les taxes
+		if ( montantTotal > 100 || listeClients.size() >= 3 ) {
+			
+			this.frais  = montantAvantTaxes * 0.15;
+			montantTotal += this.frais;
+		}
+		
+		
+		
+		
+	}
+
+	//ajoute les taxes au prix
+	public double calculerTaxes(double prix) {
+		// TODO Auto-generated method stub
+		double tauxTPS = 0.05;
+		double tauxTVQ = 0.1;
+		
+		return ((prix * tauxTPS) + (prix * tauxTVQ));
 	}
 
 
-	public void montantFinal() {
-		double prixApresTaxes, prixAvantTaxes;
-		
-		for (Clients client : listeClients) {
-			
-			
-			}
-		}
 
 }
