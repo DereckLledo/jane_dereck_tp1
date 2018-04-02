577668abbd08a81aea68d27451adeef40e4b2570
Dereck Lledo <derecklledo@gmail.com>
2018-04-02 15:52:26 -0400

    Création d'une classe Tables

---
+++ TP1/src/partie3/Tables.java
@@ -0,0 +1,23 @@
+package partie3;
+
+import java.util.ArrayList;
+
+public class Tables {
+	
+	//permet d'avoir des tables avec des numéros différents
+	
+	private ArrayList<Clients> listeClients = new ArrayList<Clients>();
+	private double montantTotal = 0;
+	private int numeroTable;
+	
+	public Tables(ArrayList<Clients> listeClients, int numeroTable){
+		
+		this.listeClients = listeClients;
+		
+		this.numeroTable = numeroTable;
+		
+	}
+
+
+
+}
